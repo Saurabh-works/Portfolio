cdf922517eb003806f71feed1ef7c00c6f80787e
Saurabh <saurabhshinde9637@gmail.com>
2024-01-24 20:32:35 +0530

    Creating a my profile

---
+++ main.js
@@ -0,0 +1,9 @@
+// var tablinks = document.getElementsByClassName("tab-links")
+// var tabcontents = document.getElementsByClassName("tab-content")
+
+// function opentab(tabname){
+//     for(tablink of tablinks){
+//         tablinks.classList.remove("active-tab");
+//     }
+// }
+
